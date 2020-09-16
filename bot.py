999bcb34a41b68172fe9d6092a48b07920ba6616
basil <basil@X>
2020-09-16 17:04:06 +0300

    make it better

--- bot.py
+++ bot.py
@@ -1,14 +1,45 @@
 import config
 import telebot
 from googletrans import Translator
+from telebot import types
+
 
 bot = telebot.TeleBot(config.token)
+
+lang = 'ru'
+
+@bot.message_handler(commands=["info"])
+
+def info(message):
+	bot.send_message(message.chat.id, "/setLanguage < Choose language", message.text)
+
+
+@bot.message_handler(commands=["setLanguage"])
+
+def any_msg(message):
+    keyboard = types.InlineKeyboardMarkup()
+    callback_button = types.InlineKeyboardButton(text="Push me", callback_data="test")
+    keyboard.add(callback_button)
+    bot.send_message(message.chat.id, "Push", reply_markup=keyboard)
+
+# В большинстве случаев целесообразно разбить этот хэндлер на несколько маленьких
+@bot.callback_query_handler(func=lambda call: True)
+def callback_inline(call):
+    # Если сообщение из чата с ботом
+    if call.message:
+        if call.data == "test":
+            bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text="Bang")
+    # Если сообщение из инлайн-режима
+    elif call.inline_message_id:
+        if call.data == "test":
+            bot.edit_message_text(inline_message_id=call.inline_message_id, text="Бдыщь")
+
 
 @bot.message_handler(content_types=["text"])
 
 def trans(message): #translates message to english
 	t = Translator()
-	bot.send_message(message.chat.id, t.translate(message.text).text)
+	bot.send_message(message.chat.id, t.translate(message.text, dest=lang).text)
 
 if __name__ == '__main__':
      bot.infinity_polling()
---
+++ utils.py
@@ -0,0 +1,5 @@
+def default_test(message):
+    keyboard = types.InlineKeyboardMarkup()
+    url_button = types.InlineKeyboardButton(text="Перейти на Яндекс", url="https://ya.ru")
+    keyboard.add(url_button)
+    bot.send_message(message.chat.id, "Привет! Нажми на кнопку и перейди в поисковик.", reply_markup=keyboard)
