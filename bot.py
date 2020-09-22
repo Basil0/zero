7bc9eb76c15475f5e448939b455499f83e497697
basil <basil@X>
2020-09-22 18:28:34 +0300

    Update

--- bot.py
+++ bot.py
@@ -2,42 +2,30 @@ import config
 import telebot
 from googletrans import Translator
 from telebot import types
-
+# GOOGLE TRANSLATE BOT by BASIL0
 
 bot = telebot.TeleBot(config.token)
 
-lang = 'ru'
+# here is a variable with a value of chosen target language
+lang = 'en'
 
 @bot.message_handler(commands=["info"])
 
 def info(message):
-	bot.send_message(message.chat.id, "/setLanguage < Choose language", message.text)
+	bot.send_message(message.chat.id, "/setLanguage <-- Choose language", message.text)
 
 
 @bot.message_handler(commands=["setLanguage"])
 
-def any_msg(message):
+def set_language(message):
     keyboard = types.InlineKeyboardMarkup()
-    callback_button = types.InlineKeyboardButton(text="Push me", callback_data="test")
+    callback_button = types.InlineKeyboardButton(text=lang, callback_data=lang)
     keyboard.add(callback_button)
-    bot.send_message(message.chat.id, "Push", reply_markup=keyboard)
-
-# В большинстве случаев целесообразно разбить этот хэндлер на несколько маленьких
-@bot.callback_query_handler(func=lambda call: True)
-def callback_inline(call):
-    # Если сообщение из чата с ботом
-    if call.message:
-        if call.data == "test":
-            bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text="Bang")
-    # Если сообщение из инлайн-режима
-    elif call.inline_message_id:
-        if call.data == "test":
-            bot.edit_message_text(inline_message_id=call.inline_message_id, text="Бдыщь")
-
+    bot.send_message(message.chat.id, "This option is not avalible at the moment", reply_markup=keyboard)
 
 @bot.message_handler(content_types=["text"])
 
-def trans(message): #translates message to english
+def trans(message): # translates message to english using Translator() from googletrans module
 	t = Translator()
 	bot.send_message(message.chat.id, t.translate(message.text, dest=lang).text)
 
